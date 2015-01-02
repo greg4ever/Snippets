ade5746ee8cf72fac2f11139575fc70fcd6e850e
Greg <gberger.perso@gmail.com>
2015-01-02 20:32:00 +0800

    Add disposable

---
+++ Snippets/SnippetsTest/Basics/DisposableFinalizer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Snippets.Basics
+{
+    public sealed class X : IDisposable
+    {
+        private bool _disposed;
+        private FileStream _resourceUnmanaged;
+        private SafeHandle _resourceManaged;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    if (_resourceManaged != null)
+                        _resourceManaged.Dispose();
+                }
+
+                if (_resourceUnmanaged != null)
+                    _resourceUnmanaged.Dispose();
+
+                _disposed = true;
+            }
+        }
+
+        ~X()
+        {
+            Dispose(false);
+        }
+    }
+
+    public class Y : IDisposable
+    {
+        private bool _disposed;
+        private FileStream _resourceUnmanaged;
+        private SafeHandle _resourceManaged;
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    if (_resourceManaged != null)
+                        _resourceManaged.Dispose();
+                }
+
+                if (_resourceUnmanaged != null)
+                    _resourceUnmanaged.Dispose();
+
+                _disposed = true;
+            }
+        }
+
+        ~Y()
+        {
+            Dispose(false);
+        }
+    }
+
+    public class Z : Y
+    {
+        private bool _disposed;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+
+                }
+
+                _disposed = true;
+            }
+
+            base.Dispose();
+        }
+
+        ~Z()
+        {
+            Dispose(false);
+        }
+    }
+}
---
+++ Snippets/SnippetsTest/Basics/DisposableSimple.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SnippetsTest.Basics
+{
+    // To handle several calls to Dispose
+    // we could add disposed boolean or even raise ObjectDisposedException
+    public sealed class A : IDisposable
+    {
+        private SafeHandle _resource;
+
+        public void Dispose()
+        {
+            if (_resource != null)
+                _resource.Dispose();
+        }
+    }
+
+    public class B : IDisposable
+    {
+        private bool _disposed;
+        private SafeHandle _resource;
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    if (_resource != null)
+                        _resource.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
+    }
+
+    public class C : B
+    {
+        private bool _disposed;
+        private SafeHandle _resource;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    if (_resource != null)
+                        _resource.Dispose();
+                }
+
+                _disposed = true;
+            }
+
+            base.Dispose();
+        }
+    }
+}
