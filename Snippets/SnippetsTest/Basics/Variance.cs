799bc9bb5e8ac63f60bd89ea90adc4ed1ea53f5e
Greg <gberger.perso@gmail.com>
2015-01-04 20:50:42 +0800

    Variance

---
+++ Snippets/SnippetsTest/Basics/Variance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SnippetsTest.Basics
+{
+    interface IMyVariance<in T, out R>
+    {
+        R MyFunction(T input);
+    }
+
+    class MyVariance<T, R> : IMyVariance<T, R>
+    {
+        public R MyFunction(T input)
+        {
+            return default(R);
+        }
+    }
+
+    public delegate R MyDelegate<in T, out R>(T input);
+
+    public class Test
+    {
+        public void VarianceTest()
+        {
+            IMyVariance<ArgumentException, Exception> test = new MyVariance<Exception, ArgumentException>();
+            MyDelegate<ArgumentException, Exception> MyDel = x => null;
+            MyDelegate<Exception, ArgumentException> MyDel2 = x => null;
+            MyDel = MyDel2;
+        }
+    }
+}
