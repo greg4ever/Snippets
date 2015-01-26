00f5c169801ff848fdb6a00f28141e3854059499
Greg <gberger.perso@gmail.com>
2015-01-27 00:23:57 +0800

    Cast

---
+++ Snippets/SnippetsTest/Basics/Cast.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SnippetsTest.Basics
+{
+    [TestClass]
+    class Cast
+    {
+        class Base { }
+        class Extended : Base { }
+        class Other { }
+
+        public void Misc()
+        {
+            var a = (Exception)null; // valid
+
+            var b = new Base();
+            var c = (Extended)b;
+            // var d = (Other)b; does not compile, safe type check
+            // var d = b as Other; does not compile neither
+        }
+    }
+}
