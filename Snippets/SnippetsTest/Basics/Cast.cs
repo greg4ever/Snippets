d7eeffe02e4b9e4a2a2061bf6b99039d66d0b705
Greg <gberger.perso@gmail.com>
2015-01-27 23:22:47 +0800

    misc

--- Snippets/SnippetsTest/Basics/Cast.cs
+++ Snippets/SnippetsTest/Basics/Cast.cs
@@ -7,7 +7,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace SnippetsTest.Basics
 {
     [TestClass]
-    class Cast
+    public class Cast
     {
         class Base { }
         class Extended : Base { }
---
+++ Snippets/SnippetsTest/Basics/Struct.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SnippetsTest.Basics
+{
+    [TestClass]
+    public class Struct
+    {
+        struct A
+        {
+            public int AA { get; set; }
+            public double BB { get; set; }
+
+            public A(int a)
+            {
+                AA = a;
+            }
+
+            public void Run()
+            {
+            }
+        }
+
+        [TestMethod]
+        public void CannotUseUnitializedMember()
+        {
+            A aa;
+            // var tmp = aa.AA; does not compile - use of unassigned local variable aa
+            // aa.Run(); same
+            // aa.AA = 10; same
+        }
+    }
+}
