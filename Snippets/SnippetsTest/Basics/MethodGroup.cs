de73d21ce83a0552c3a064fcc61389d3acb3f2b8
Greg <gberger.perso@gmail.com>
2015-03-04 23:35:56 +0800

    Update

---
+++ Snippets/SnippetsTest/Basics/MethodGroup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SnippetsTest.Basics.MethodGroup
+{
+    class Utils
+    {
+        public static void Test(int a) { }
+        public static void Test(double a) { }
+        public static void Test(string a) { }
+    }
+
+    public class MyClass
+    {
+        public void Run(Action<string> param) { }
+    }
+
+    public class Test
+    {
+        public void Run()
+        {
+            new MyClass().Run(Utils.Test);
+        }
+    }
+}
