541313fc068ac007f8496c01f3dae7a634dd8229
Greg <gberger.perso@gmail.com>
2015-03-04 23:29:07 +0800

    Update

---
+++ Snippets/SnippetsTest/Basics/Inheritance.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace SnippetsTest.Basics.Inheritance
+{
+    class A
+    {
+        public virtual void Function(int a)
+        {
+            Debug.WriteLine("A.Int");
+        }
+    }
+
+    class B : A
+    {
+        public override void Function(int a)
+        {
+            Debug.WriteLine("B.Int");
+        }
+
+        public void Function(double a)
+        {
+            Debug.WriteLine("B.Double");
+        }
+    }
+
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        public void Run()
+        {
+            B a = new B();
+            a.Function(1); // display B.Double
+        }
+    }
+}
