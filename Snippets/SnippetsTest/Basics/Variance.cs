872a52b7234858e1aa99e78aa408014cc444f204
Greg <gberger.perso@gmail.com>
2015-03-08 20:18:27 +0800

    Update

--- Snippets/SnippetsTest/Basics/Variance.cs
+++ Snippets/SnippetsTest/Basics/Variance.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace SnippetsTest.Basics
 {
+    // Only interfaces & delegates support variance
+
     // T: in = contravariance
     // R: out = covariance
     interface IMyVariance<in T, out R>
@@ -22,8 +25,10 @@ namespace SnippetsTest.Basics
 
     public delegate R MyDelegate<in T, out R>(T input);
 
+    [TestFixture]
     public class Test
     {
+        [Test]
         public void VarianceTest()
         {
             IMyVariance<ArgumentException, Exception> test = new MyVariance<Exception, ArgumentException>();
@@ -31,6 +36,21 @@ namespace SnippetsTest.Basics
             MyDelegate<ArgumentException, Exception> MyDel = x => { return null; };
             MyDelegate<Exception, ArgumentException> MyDel2 = x => null;
             MyDel = MyDel2;
+        }
+
+        [Test]
+        public void ArrayCovariance()
+        {
+            Exception[] array = new ArgumentException[10];
+            array[0] = new Exception(); // raise ArrayTypeMismatchException, cause the backing store is type of ArgumentException
+        }
+        
+        [Test]
+        public void ListCovariance()
+        {
+            // List<Exception> list = new List<ArgumentException>(); list is not covariant
+            // public interface IEnumerable<out T> : IEnumerable
+            IEnumerable<Exception> ienum = new List<ArgumentException>(); // IEnumerable<T> is covariant
         }
     }
 }
