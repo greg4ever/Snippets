6ac6676a48c7fb5c9e3361fdcfb4d16be8396569
Greg <gberger.perso@gmail.com>
2015-03-08 21:09:28 +0800

    Update

---
+++ Snippets/SnippetsTest/Basics/NamedParameter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace SnippetsTest.Basics.NamedParameter
+{
+    public class MyClass
+    {
+        public void Test(int a, int b, int c) { }
+    }
+
+    [TestFixture]
+    class TestClass
+    {
+        [Test]
+        public void Run()
+        {
+            var c = new MyClass();
+            c.Test(c:3, b:2, a:1);
+        }
+    }
+}
