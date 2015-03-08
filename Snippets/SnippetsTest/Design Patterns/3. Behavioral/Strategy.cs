fb38c16195a767c98f2e0a1f8a329c62ed330a4f
Greg <gberger.perso@gmail.com>
2015-03-08 22:29:06 +0800

    Add strategy

---
+++ Snippets/SnippetsTest/Design Patterns/3. Behavioral/Strategy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SnippetsTest.Design_Patterns._3._Behavioral
+{
+    interface IPricer
+    {
+        double Price();
+    }
+
+    class VanillaOptionPricer : IPricer
+    {
+        public double Price()
+        {
+            return 10;
+        }
+    }
+
+    class BarrierOptionPricer : IPricer
+    {
+        public double Price()
+        {
+            return 20;
+        }
+    }
+
+    class PricerContext
+    {
+        private readonly IPricer _pricer = new VanillaOptionPricer();
+
+        public void PriceMe()
+        {
+            _pricer.Price();
+        }
+    }
+}
