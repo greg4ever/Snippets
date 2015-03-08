d36600a778ba49ec1850559a3f82d034c94785b7
Greg <gberger.perso@gmail.com>
2015-03-08 23:52:46 +0800

    AsyncAwait

---
+++ Snippets/SnippetsTest/Basics/AsyncAwait.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SnippetsTest.Basics
+{
+    [TestFixture]
+    class TestClass
+    {
+        [Test]
+        public async void Run()
+        {
+            bool value = false;
+            var task = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(5000);
+                value = true;
+            });
+            await task;
+
+            Assert.IsTrue(value);
+        }
+    }
+}
