ccebf8248ad15f4e57b4502e4646d95f9ff5f1d8
Greg <gberger.perso@gmail.com>
2015-03-11 20:21:00 +0800

    Update

---
+++ Snippets/SnippetsTest/Basics/TPL/Basics.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SnippetsTest.Basics.TPL
+{
+    [TestFixture]
+    class Basics
+    {
+        [Test]
+        // Promise ~= Future
+        // Future result is set by someone else
+        // Promise result can be set by the user
+        public void RunFuture()
+        {
+            Task<int> future = Task.Factory.StartNew<int>(() =>
+            {
+                Thread.Sleep(2000);
+                return 1;
+            });
+
+            var res = future.Result;
+        }
+    }
+}
