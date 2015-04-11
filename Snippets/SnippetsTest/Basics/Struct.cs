cfbb0e63da894e91cbe9689010f43b0c4899bed6
Greg <gberger.perso@gmail.com>
2015-04-11 23:28:45 +0800

    Misc

--- Snippets/SnippetsTest/Basics/Struct.cs
+++ Snippets/SnippetsTest/Basics/Struct.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
@@ -27,6 +28,29 @@ namespace SnippetsTest.Basics
             // var tmp = aa.AA; does not compile - use of unassigned local variable aa
             // aa.Run(); same
             // aa.AA = 10; same
+        }
+
+        class B
+        {
+            public class MyPoint
+            {
+                public int X;
+                public int Y;
+            }
+
+            private Point _point;
+            private MyPoint _myPoint;
+
+            public Point Point { get; set; }
+            public MyPoint PointClass { get; set; }
+        }
+
+        [Test]
+        public void CompilerErrorWhenStructProperty()
+        {
+            var b = new B();
+            // b.Point.X += 5; // cannot modify the expression because it is not a variable
+            b.PointClass.X += 5;
         }
     }
 }
---
+++ Snippets/SnippetsTest/Exo/NumberOfWordsInSentence.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SnippetsTest.Exo
+{
+    [TestFixture]
+    class NumberOfWordsInSentence
+    {
+        [Test]
+        public void Test()
+        {
+            var sentence = "a aa b ccc test ccc c aa a aa";
+
+            // LINQ
+            {
+                var ordered = sentence.Split()
+                    .GroupBy(x => x).Select(x => new { Word = x.Key, Occurence = x.Count() })
+                    .OrderByDescending(x => x.Occurence).ThenBy(x => x.Word);
+            }
+
+            // Imperative
+            {
+                var dicoCount = new Dictionary<string, int>();
+                foreach (var word in sentence.Split())
+                {
+                    int count;
+                    if (dicoCount.TryGetValue(word, out count))
+                        dicoCount[word]++;
+                    else
+                        dicoCount[word] = 1;
+                }
+
+                var list = dicoCount.ToList();
+                list.Sort((a, b) => -a.Value.CompareTo(b.Value) * 2 + a.Key.CompareTo(b.Key));
+            }
+
+            // Imperative 2
+            {
+                var dico = new Dictionary<string, int>();
+
+                foreach (var word in sentence.Split())
+                {
+                    int count;
+                    if (dico.TryGetValue(word, out count))
+                        dico[word]++;
+                    else
+                        dico[word] = 1;
+                }
+
+                var sortedDico = new SortedSet<DicoWrapper>(new DicoWrapper());
+                foreach (var item in dico)
+                    sortedDico.Add(new DicoWrapper() { Occurence = item.Value, Word = item.Key });
+            }
+        }
+
+        class DicoWrapper : IComparer<DicoWrapper>
+        {
+            public string Word { get; set; }
+            public int Occurence { get; set; }
+
+            public int Compare(DicoWrapper a, DicoWrapper b)
+            {
+                return -a.Occurence.CompareTo(b.Occurence) * 2 + a.Word.CompareTo(b.Word);
+            }
+        }
+    }
+}
