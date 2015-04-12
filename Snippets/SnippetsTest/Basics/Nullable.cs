fc017285e921c27e5f6613db1c7ddc22ea2d8019
Greg <gberger.perso@gmail.com>
2015-04-12 23:10:33 +0800

    Misc

---
+++ Snippets/SnippetsTest/Basics/Nullable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SnippetsTest.Basics
+{
+    [TestFixture]
+    class Nullable
+    {
+        [Test]
+        public void LiftedOperator()
+        {
+            var i = 5;
+            // compile thanks to lifted operator. i is boxed into a Nullable<int> and compared to null.
+            var res = i == null; 
+        }
+    }
+}
